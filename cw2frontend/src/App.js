081ec3249cd6bd722212f3979b167bd29d8c9a35
TommyLeong1 <122337479+TommyLeong1@users.noreply.github.com>
2023-06-18 12:05:08 +0000

    edit App.js

--- cw2frontend/src/App.js
+++ cw2frontend/src/App.js
@@ -1,13 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Space } from 'antd';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './screen/Home';
 import Employee from './screen/Employee'
 
-
-
+const { Header, Content, Footer } = Layout;
 function App() {
-  return (
-    <Employee/>
-  );
+ 
+	return (
+		<Layout>
+			<Router>
+				<Header> 
+					<nav>
+            <Space>
+						<Link to="/">Home</Link>
+            </Space>
+					</nav>          
+				</Header>
+				<Content style={{ padding: '0 50px', height: '90%' }}>
+					<Routes>
+						<Route exact path="/" element={<Home />} />
+					</Routes>
+				</Content>
+				<Footer>
+					<p style={{ color: 'green' }}>VT6003CEM Demo</p>
+				</Footer>
+			</Router>
+		</Layout>
+	);
 }
 
 export default App;
