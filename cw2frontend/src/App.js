24bd7beb1fdedb7887f631dc168cc4ae20d46997
TommyLeong1 <122337479+TommyLeong1@users.noreply.github.com>
2023-06-19 13:42:44 +0000

    final commit

--- cw2frontend/src/App.js
+++ cw2frontend/src/App.js
@@ -4,7 +4,14 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Home from './screen/Home';
 import Login from './screen/Login';
+import Register from './screen/Register';
+import Admin from './screen/Admin'
 import Employee from './screen/Employee'
+import User from './screen/User'
+import EditCat from './screen/EditCat'
+import EditFavCat from './screen/EditFavCat'
+import FavCatPost from './screen/FavCatPost'
+import MASboard from './screen/MASboard'
 
 const { Header, Content, Footer } = Layout;
 function App() {
@@ -16,17 +23,30 @@ function App() {
 					<nav>
             <Space>
 						<Link to="/">Home</Link>
-              <Link to="./screen/Login">Sign in/Sign up</Link>
-              <Link to="/">Profile</Link>
-              <Link to="/">Favourites</Link>
-              <Link to="/">Message board</Link>
+              <Link to="/Register">Sign up</Link>
+              <Link to="/Login">Sign in</Link>
+              <Link to="/Admin">Edit Admin</Link>
+              <Link to="/Employee">Edit Employee</Link>
+              <Link to="/User">Edit User</Link>
+              <Link to="/EditCat">Edit Cat post</Link>
+              <Link to="/EditFavCat">Edit Favourites Cat post</Link>
+              <Link to="/FavCatPost">Favourites Cat</Link>
+              <Link to="/MASboard">Message board</Link>
             </Space>
 					</nav>          
 				</Header>
 				<Content style={{ padding: '0 50px', height: '90%' }}>
 					<Routes>
 						<Route exact path="/" element={<Home />} />
-            <Route path="./screen/Login" element={<Login />}  /> 
+            <Route path="/Register" element={<Register />}  /> 
+            <Route path="/Login" element={<Login />}  /> 
+            <Route path="/Admin" element={<Admin />}  /> 
+            <Route path="/Employee" element={<Employee />}  /> 
+            <Route path="/User" element={<User />}  /> 
+            <Route path="/EditCat" element={<EditCat />}  /> 
+            <Route path="/EditFavCat" element={<EditFavCat />}  /> 
+            <Route path="/FavCatPost" element={<FavCatPost />}  />
+            <Route path="/MASboard" element={<MASboard />}  />
 					</Routes>
 				</Content>
 				<Footer>
