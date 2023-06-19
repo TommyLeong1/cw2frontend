309e8f8ce80cdeb713df28f990a16b86e668072a
TommyLeong1 <122337479+TommyLeong1@users.noreply.github.com>
2023-06-19 08:20:04 +0000

    edit login.js

--- cw2frontend/src/screen/Login.js
+++ cw2frontend/src/screen/Login.js
@@ -1,76 +1,71 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { useState } from 'react';
+import { Form, Input, Button, message, Select } from 'antd';
+import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
 
-const Login = () => {
-  const userRef = useRef();
-  const errRef = useRef();
+const { Option } = Select;
 
-  const [user, setUser]= useState('');
-  const [pwd, setPwd]= useState('');
-  const [errMsg, setErrMsg]= useState('');
-  const [success, setSuccess]= useState(false);
+const API_URL = 'https://cw2backend.tommyleong1.repl.co';
 
-  useEffect(() => {
-        userRef.current.focus();
-    }, [])
-
-  useEffect(() => {
-        setErrMsg('');
-    }, [user,pwd])
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(user, pwd);
-    setUser('');
-    setPwd('');
-    setSuccess(true);
-  }
-  
-  return (
-    <>
-      {success ? (
-        <session>
-          <h1>You are logged in!</h1>
-          <br />
-          <p>
-            <a href="/Home">Back to Home</a>
-          </p>
-        </session>
-      ) : (
-    <section>
-      <p ref={errRef} className={errMsg ? "errmsg" : "offscreen" }         aria-live="assertive">{errMsg}</p>
-      <h1>Sign In</h1>
-      <form onSubmit={handleSubmit}>
-          <label htmlFor="username">Username:</label>
-          <input
-              type="text" 
-              id="username"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setUser(e.target.value)}
-              value={user}
-              required
-          />
-
-          <label htmlFor="password">Password:</label>
-          <input 
-              type="password" 
-              id="password"
-              onChange={(e) => setUser(e.target.value)}
-              value={pwd}
-              required
-          />
-          <button>Sign In</button>
-      </form>
-      <p>
-          Need an Account?<br />
-          <span className="line">
-              <a herf="#">Sign Up </a>
-          </span>
-      </p>
-    </section>
-      )}
-    </>
-  )
+interface LoginFormValues {
+  username: string;
+  password: string;
+  role: string;
 }
 
-export default Login
+const Login = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async (values: LoginFormValues) => {
+    setLoading(true);
+
+    try {
+      const response = await axios.post(`${API_URL}/login`, values);
+      localStorage.setItem('token', response.data.token);
+      message.success('Login successful');
+    } catch (error) {
+      message.success('Login successful');
+      <Route exact path="/" element={<Home />} />
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <Form onFinish={handleLogin}>
+      <Form.Item
+        name="username"
+        label="Username"
+        rules={[{ required: true, message: 'Please enter your username' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[{ required: true, message: 'Please enter your password' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item
+        name="role"
+        label="Role"
+        rules={[{ required: true, message: 'Please select your role' }]}
+      >
+        <Select>
+          <Option value="admin">Admin</Option>
+          <Option value="employee">Employee</Option>
+          <Option value="user">User</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" loading={loading}>
+          Login
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Login;
